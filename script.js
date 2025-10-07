af53d893de5c64a1372027fe45842faf1fb8606c
JRLapierre <jeanremi.lapierre@gmail.com>
2025-10-07 11:55:18 +0200

    Functionning version with timer-box as a web component

--- script.js
+++ script.js
@@ -24,38 +24,13 @@ document.addEventListener('DOMContentLoaded', function() {
 //-----------------------------------------------------
 //initialisation
 
-function createBox(id) {
-    /*`
-        <div class="area">
-            <div class="shapebox" id="shapebox1" style="right: 0px;">
-                <span class="spanBox">4</span>
-                <div class="colorbox" id="colorbox1"></div>
-            </div>
-        </div>
-    `*/
-    //create from the bottom
-    const newColorbox = document.createElement('div');//the color
-    newColorbox.className = "colorbox";
-    newColorbox.id = "colorbox"+id;
-    const newSpanbox = document.createElement('span');//the number
-    newSpanbox.className = "spanBox";
-    newSpanbox.textContent = (nbBoxes - id) + 1;
-    const newShapebox = document.createElement('div');//the box
-    newShapebox.className = "shapebox";
-    newShapebox.id = "shapebox"+id;
-    newShapebox.style = ((currentQuestion + id) % 2 == 0) ? "left:0" : "right:0";
-    newShapebox.appendChild(newSpanbox);
-    newShapebox.appendChild(newColorbox);
-    const newArea = document.createElement('div');//the area
-    newArea.className = "area";
-    newArea.appendChild(newShapebox);
-    return newArea;
-}
-
 function createTimer() {
     const container = document.getElementById("timer");
     for (let i = 1; i <= nbBoxes; i++) {
-        container.appendChild(createBox(i));
+        const box = document.createElement('timer-box');
+        box.id = 'timerbox'+i;
+        box.setAttribute('value', nbBoxes - i + 1);
+        container.appendChild(box);
     }
     reset()
 }
@@ -68,7 +43,7 @@ function drainLiquid() {
     // Reduce height by a small percentage
     if (liquidHeight > 0) { 
         liquidHeight -= 1; 
-        document.getElementById('colorbox'+boxNumber).style.height = liquidHeight + '%';
+        document.getElementById('timerbox'+boxNumber).drain1();
     }
     else if (boxNumber < nbBoxes) {
         boxNumber += 1;
@@ -89,8 +64,9 @@ function reset() {
     currentQuestion += 1;
     if (intervalId) clearInterval(intervalId);
     for (let i = 1; i <= nbBoxes; i++) {
-        document.getElementById('colorbox'+i).style.height = '100%';
-        document.getElementById('shapebox'+i).style = ((currentQuestion + i) % 2 == 0) ? "left:0" : "right:0";
+        const box = document.getElementById('timerbox'+i)
+        box.resetHeight();
+        box.setSide((currentQuestion + i) % 2 == 0);
     }
     liquidHeight = 100;
     boxNumber = 1;
@@ -116,10 +92,10 @@ function pause() {
     intervalId = 0;
     overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
     resetEvents();
-    const shapeBox = document.getElementById('shapebox'+boxNumber);
+    const timerbox = document.getElementById('timerbox'+boxNumber);
     const sideOverlay = document.getElementById("sideOverlay");
-    sideOverlay.style.left = shapeBox.style.left;
-    sideOverlay.style.right = shapeBox.style.right;
+    sideOverlay.style.left = timerbox.getPosition().left;
+    sideOverlay.style.right = timerbox.getPosition().right;
     document.getElementById("leftOverlay").innerHTML = '&#10004;';
     document.getElementById("rightOverlay").innerHTML = '&#10060;';
     setTimeout(() => {
@@ -138,32 +114,14 @@ function resetEvents() {
     overlay.removeEventListener('click', play);
 }
 
-//---------------------------------------------------------
-//shifting cases functions
-
-
-function shiftBox() {
-    const shapeBox = document.getElementById('shapebox'+boxNumber);
-    //if the box in on the left
-    if (shapeBox.style.left === "") {
-        shapeBox.style.left = '0px';
-        shapeBox.style.right = "";
-    }
-    //if the box is on the right
-    else {
-        shapeBox.style.left = "";
-        shapeBox.style.right = '0px';
-    }
-}
-
 //--------------------------------------------------------------
 //answering questions
 
 function rightAnswer() {
     //give the points to the team
-    const shapeBox = document.getElementById("shapebox"+boxNumber)
-    const newPoints = shapeBox.getElementsByTagName("span")[0].textContent;
-    const teamId = shapeBox.style.right === "" ? "team1score" : "team2score";
+    const box = document.getElementById("timerbox"+boxNumber)
+    const newPoints = box.value;
+    const teamId = box.isLeftSide() ? "team1score" : "team2score";
     const spanElement = document.getElementById(teamId);
     spanElement.textContent = parseInt(spanElement.textContent) + parseInt(newPoints);
     //reset
@@ -172,7 +130,7 @@ function rightAnswer() {
 
 function wrongAnswer() {
     //shift the current case
-    shiftBox();
+    document.getElementById('timerbox'+boxNumber).shiftSide();
     //continuer
     play();
 }
---
+++ timer-box.js
@@ -0,0 +1,118 @@
+class TimerBox extends HTMLElement {
+
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: 'open' });
+        this.liquidHeight = 100;
+        this._value = 0;
+        this.nbSwitch = 0;
+
+        //create the colorbox
+        this.colorbox = document.createElement('div');//the color
+        Object.assign(this.colorbox.style, {
+            width: '150px',
+            height: '100%',
+            position: 'absolute',
+            bottom: '0',
+            backgroundColor: 'orange',
+            zIndex: '1'
+        });
+
+        //create the spanbox
+        this.spanbox = document.createElement('span');//the number
+        Object.assign(this.spanbox.style, {
+            position: 'relative',
+            fontFamily: 'sans-serif',
+            fontSize: '130px',
+            fontWeight: 'bolder',
+            color: 'yellow',
+            zIndex: '2'
+        });    
+        
+        //create the shapebox
+        this.shapebox = document.createElement('div');//the box
+        Object.assign(this.shapebox.style, {
+            width: '150px',
+            height: '150px',
+            position: 'absolute',
+            bottom: '0',
+            border: '3px solid gainsboro',
+            textAlign: 'center',
+            borderRadius: '10px'
+        })
+
+        //create the containing area
+        this.area = document.createElement('div');//the area
+        Object.assign(this.area.style, {
+            width: '275px',
+            height: '153px',
+            display: 'flex',
+            position: 'relative'
+        })
+
+        //combine the elements
+        this.shapebox.appendChild(this.spanbox);
+        this.shapebox.appendChild(this.colorbox);
+        this.area.appendChild(this.shapebox);
+        shadow.appendChild(this.area);
+
+        this.updateSide();
+    }
+
+    static get observedAttributes() {
+        return ['value'];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'value') {
+            this._value = parseInt(newValue) || 0;
+            this.updateBox();
+        }
+    }
+
+    get value() { return this._value; }
+
+    getPosition() {
+        return {
+            left: this.shapebox.style.left,
+            right: this.shapebox.style.right
+        };
+    }
+
+    setSide(isLeft) {
+        this.shapebox.style.left = isLeft ? '0px' : 'auto';
+        this.shapebox.style.right = isLeft ? 'auto' : '0px';
+    }
+
+    updateSide() {
+        const isLeft = (this._value + this.nbSwitch) % 2 === 0;
+        this.shapebox.style.left = isLeft ? '0px' : 'auto';
+        this.shapebox.style.right = isLeft ? 'auto' : '0px';
+    }
+
+    updateBox() {
+        this.updateSide();
+        this.spanbox.textContent = this._value;
+    }
+
+    drain1() {
+        this.liquidHeight -= 1; 
+        this.colorbox.style.height = this.liquidHeight + '%';
+    }
+
+    resetHeight() {
+        this.liquidHeight = 100;
+        this.colorbox.style.height = this.liquidHeight + '%';
+    }
+
+    shiftSide() {
+        this.nbSwitch++;
+        this.updateSide();
+    }
+
+    isLeftSide() {
+        return this.shapebox.style.left !== 'auto';
+    }
+}
+
+customElements.define('timer-box', TimerBox);
